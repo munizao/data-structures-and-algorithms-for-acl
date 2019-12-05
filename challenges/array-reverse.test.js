ab79e50ef06db8992121a51f9f03d4637b511f0f
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-05 13:27:29 -0800

    two tests for even and odd length arrays

--- challenges/array-reverse.test.js
+++ challenges/array-reverse.test.js
@@ -2,3 +2,12 @@
   arrayReverse
 } = require('./array-reverse');
 
+describe ('array reverse module', () => {
+  it('reverses an even length array', () => {
+    expect(arrayReverse([1, 2, 3, 4])).toEqual([4, 3, 2, 1]);
+  });
+  it('reverses an odd length array', () => {
+    expect(arrayReverse([1, 2, 3])).toEqual([3, 2, 1]);
+  });
+
+};
