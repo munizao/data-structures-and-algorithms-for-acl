b36c58d6a75ec0a3093faa577d48d942f706d816
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-07 15:00:43 -0800

    wrote insertShiftArray function

--- challenges/array-shift.js
+++ challenges/array-shift.js
@@ -1,4 +1,13 @@
-function insertShiftArray(array) {
+function insertShiftArray(array, valueToInsert) {
+  const newArray = new Array(array.length + 1);
+  const insertPosition = Math.ceil(array.length / 2);
+  for(let i = 0; i <= insertPosition; i++) {
+    newArray[i] = array[i];
+  }
+  newArray[insertPosition] = valueToInsert;
+  for(let i = insertPosition; i < array.length; i++) {
+    newArray[i + 1] = array[i];
+  }
 
   return newArray;
 }
