b79d1b364b450b539a4e2d9ddcf1cdf734bb4f9b
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-07 16:14:17 -0800

    wrote node and linked list classes

---
+++ data-structures/linked-list/linked-list.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+module.exports = {
+  Node,
+  LinkedList
+}
