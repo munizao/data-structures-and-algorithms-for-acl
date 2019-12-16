d502804ef25906d41651e5769a578f818fc89ea3
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-15 22:23:02 -0800

    fixed case of shorter first list

--- data-structures/ll-merge/ll-merge.js
+++ data-structures/ll-merge/ll-merge.js
@@ -1,5 +1,3 @@
-const { LinkedList } = require('../linked-list/linked-list');
-
 function mergeLists(l1, l2) {
   let curr1 = l1.head;
   let curr2 = l2.head;
@@ -7,7 +5,9 @@ function mergeLists(l1, l2) {
     const nextcurr1 = curr1.next;
     const nextcurr2 = curr2.next;
     curr1.next = curr2;
-    curr2.next = nextcurr1;
+    if(nextcurr1) {
+      curr2.next = nextcurr1;
+    }
     curr1 = nextcurr1;
     curr2 = nextcurr2;
   }
